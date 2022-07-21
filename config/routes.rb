734d12786ae3655250d3edaca8d66bbe74510ac5
jonathan bola <bolajonathan@yahoo.com>
2022-07-21 16:25:14 -0500

    sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
+  post '/signup', to: 'users#create'
   
 end
