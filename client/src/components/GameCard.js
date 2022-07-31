a2405bde382b6a42c82195ac1c762efae12c3251
jonathan bola <bolajonathan@yahoo.com>
2022-07-31 16:58:51 -0500

    Card.js

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './GameCard.css'
 
-export default function GameCard() {
-  return (
-    <div>GameCard</div>
-  )
+
+export default function GameCard({game, setGameUser, setGameComments}) {
+  const [btn, setBtn] = useState(true)
+//   const [reviews, setReviews] = useState([])
+  function handleClick(){
+   console.log(game.reviews) 
+   console.log(game.users)
+    setBtn(!btn)    
+  }
+  return<>
+  <div className='card'>
+  
+  <h4>{game.title}</h4>
+      <h3>{game.year}</h3>
+     <img className='card-img' src={game.image_url} alt={game.title}/>
+
+    
+    <button className="card-btn" onClick={handleClick} style={{background: !btn? "rgba(0, 0, 0, 0.5)": "#000"}}>{!btn?"Hide Reviews": "Show Reviews"}</button>
+  </div>
+  
+  
+  </>
 }
--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect}from 'react'
 import GameCard from './GameCard';
 import SearchBar from './SearchBar';
+import './GameList.css'
 
 export default function GameList() {
 
   const [games, setGames] = useState([])
+  const [gameUser, setGameUser] = useState("")
+  const [gameComments, setGameComments] = useState("")
   useEffect(() => {
     fetch("/games")
       .then((r) => r.json())
@@ -13,11 +16,29 @@ export default function GameList() {
         console.log(games)
       });
   }, []);
-  return (
-    <div>
-        <div><SearchBar/></div>
-        {games.map((game)=>{
-            return <GameCard/>
-        })}GameList</div>
+  function handleClickCard(){
+
+  }
+  return (<div>
+            <div id="myNav" className="overlay">
+
+              
+                <a href="javascript:void(0)" className="closebtn" onclick="closeNav()">&times;</a>
+
+               
+                <div className="overlay-content">
+                    <h2>{gameUser}</h2>
+                    <p>{gameComments}</p>
+                </div>
+
+            </div>
+
+            <div id='search'><SearchBar/></div>
+            <div className='container'>{games.map((game)=>{
+                return <GameCard game={game} key={game.id} setGameUser={setGameUser} setGameComments={setGameComments}/>
+            })}GameList
+            </div>
+        </div>
+        
   )
 }
