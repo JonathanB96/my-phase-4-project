e3187feb05b696e981106dd37f5737f30b81a9f8
jonathan bola <bolajonathan@yahoo.com>
2022-08-16 17:01:44 -0500

    custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/hello', to: 'application#hello_world'
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  post '/reviews', to: 'reviews#create'
-  patch '/reviews/:id', to: 'reviews#update'
-  delete '/logout', to: 'sessions#destroy'
-  delete '/reviews/:id', to: 'reviews#destroy'
-  get '/games', to: 'games#index'
-  get '/reviews', to: 'reviews#index'
-  get '/users', to: 'users#index'
-  get '/games/:id/reviews', to: 'games#show'
+  namespace :api do
+    get '/hello', to: 'application#hello_world'
+    post '/signup', to: 'users#create'
+    get '/me', to: 'users#show'
+    post '/login', to: 'sessions#create'
+    post '/reviews', to: 'reviews#create'
+    patch '/reviews/:id', to: 'reviews#update'
+    delete '/logout', to: 'sessions#destroy'
+    delete '/reviews/:id', to: 'reviews#destroy'
+    get '/games', to: 'games#index'
+    get '/reviews', to: 'reviews#index'
+    get '/users', to: 'users#index'
+    get '/games/:id/reviews', to: 'games#show'
+
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+ 
 end
